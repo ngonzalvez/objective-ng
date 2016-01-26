cc7bcca8d986ffcb6f5948ddf269e879d7eb1576
Nicolás Gonzálvez <cristopher.gonzalvez@gmail.com>
2016-01-26 10:17:54 -0300

    Add base Directive class

---
+++ src/Directive.js
@@ -0,0 +1,22 @@
+class Directive extends Injectable {
+  static $type = 'directive';
+  static module = '';
+  static dependencies = [];
+  static tagName = '';
+
+
+  static register(directive) {
+    const injected = directive.dependencies.slice(0);
+
+    injected.push(function() {
+      const instance = Object.create(directive.prototype);
+      directive.apply(instance, arguments);
+
+      return instance;
+    });
+
+    angular
+      .module(directive.module)
+      .directive(directive.tagName, injected);
+  }
+}
