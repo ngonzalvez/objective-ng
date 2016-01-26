50f5acfaeb4dc6d118e5f549dabe5baef23381a5
Nicolás Gonzálvez <cristopher.gonzalvez@gmail.com>
2016-01-26 10:17:23 -0300

    Add Injectable class

---
+++ src/Injectable.js
@@ -0,0 +1,48 @@
+class Injectable {
+  static $deps = {};
+  static dependencies = [];
+
+  /**
+   * Inject the injectable dependencies into the new instance and call the
+   * initialization method.
+   * @constructor
+   */
+  constructor() {
+    this.registerDependencies.apply(this, arguments);
+
+    // Pass the injector to the init function.
+    this.init(this.inject.bind(this));
+  }
+
+  init() {
+    // To be implemented in sub-class.
+  }
+
+  registerDependencies() {
+    this.constructor.dependencies.forEach((name, i) => {
+      this.constructor.$deps[name] = arguments[i];
+    });
+  }
+
+  /**
+   * Dependency injector helper function.
+   */
+  inject(depName) {
+    const dependency = this.constructor.$deps[depName];
+    // $ng.debug(`${ctrlName}: Injecting ${depName}`);
+
+    if (dependency === null || dependency === undefined) {
+      const clsName = this.constructor.name;
+      const type = this.constructor.$type;
+      const errorMsg =
+        `"${clsName}" got undefined when trying to inject ` +
+        `"${depName}". Please make sure that it is declared in the ` +
+        `${type} dependencies.`
+      ;
+
+      throw new Error(errorMsg);
+    }
+
+    return dependency;
+  }
+}
