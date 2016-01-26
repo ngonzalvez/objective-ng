f6aa3f3d78ac8ef00025c0d4007cf9beab6c422e
Nicolás Gonzálvez <cristopher.gonzalvez@gmail.com>
2016-01-26 10:17:44 -0300

    Add base Controller class

---
+++ src/Controller.js
@@ -0,0 +1,21 @@
+/**
+ * Base class for Angular.js controllers.
+ */
+class Controller extends Injectable {
+  static $type = 'controller';
+  static module = '';
+
+  /**
+   * Register the given controller in the corresponding angular module.
+   * @method
+   *
+   * @param {Class} ctrl    The controller class.
+   */
+  static register(ctrl) {
+    const injectedCtrl = ctrl.dependencies.slice(0).concat([ctrl]);
+
+    angular
+      .module(ctrl.module)
+      .controller(ctrl.name, injectedCtrl);
+  }
+}
