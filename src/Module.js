a03d85d0fa58e7ebb06266cfadce4042fbcb7f70
Nicolás Gonzálvez <cristopher.gonzalvez@gmail.com>
2016-01-26 10:18:03 -0300

    Add base Module class

---
+++ src/Module.js
@@ -0,0 +1,35 @@
+/**
+ *  Base class for angular modules.
+ */
+class Module {
+  static dependencies = [];
+  static configDependencies = [];
+  static runDependencies = [];
+  static moduleName = '';
+
+  /**
+   * Module configuration method.
+   */
+  static config() { /* Implement in subclass. */ }
+
+  /**
+   * Module initializer method.
+   */
+  static run() { /* Implement in subclass. */ }
+
+  /**
+   * Register this module instance in the angular module registry.
+   */
+  static register(module) {
+    const injectedConfig = module.configDependencies.slice(0);
+    const injectedRun = module.runDependencies.slice(0);
+
+    injectedConfig.push(module.config);
+    injectedRun.push(module.run);
+
+    angular
+      .module(module.moduleName, module.dependencies)
+      .config(injectedConfig)
+      .run(injectedRun);
+  }
+}
